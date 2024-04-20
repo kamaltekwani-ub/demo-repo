d4e1afa1587d08cd0611648fff9fdc0f71572f7d
kamaltekwani <kamaltek@buffalo.edu>
2024-04-20 18:12:28 -0400

    US 2002 - Division Operation Added

--- Test.java
+++ Test.java
@@ -23,6 +23,10 @@ public class Test
         return a * b;
     }
 
+    public int divide(int a, int b){
+        return a / b;
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello World !!!");
     }
