5cfa67778a7081ebb351964e663765ca7e331b06
kamaltekwani <kamaltek@buffalo.edu>
2024-04-20 17:05:18 -0400

    User Story 2001 Changes - Subtract Method

--- Test.java
+++ Test.java
@@ -8,6 +8,10 @@ public class Test
         return a + b;
     }
 
+    public int subtract(int a, int b){
+        return a - b;
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello World !!!");
     }
