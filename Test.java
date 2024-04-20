a05728bbff229b968e49b8502f8802ad4dde077f
kamaltekwani <kamaltek@buffalo.edu>
2024-04-20 16:56:52 -0400

    User Story 1001 Changes

--- Test.java
+++ Test.java
@@ -4,6 +4,10 @@ public class Test
         System.out.println(message);
     }
 
+    public void print(){
+        System.out.println("User Story 1001");
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello World !!!");
     }
