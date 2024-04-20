5b84cd01581c02702132678afa1b907a05abf632
kamaltekwani <kamaltek@buffalo.edu>
2024-04-20 16:45:44 -0400

    Changes - Added Print Method

--- Test.java
+++ Test.java
@@ -1,7 +1,11 @@
 public class Test
 {
+    public void print(String message) {
+        System.out.println(message);
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello World !!!");
-        System.out.println("Environment - Development");
+        // System.out.println("Environment - Development");
     }
 }
